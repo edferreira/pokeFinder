8367913f993425afbbba3ffbc6bdb8e08de1e78a
Eduardo Ferreira <eduardo@sthima.com.br>
2019-08-06 22:50:54 -0300

    finally RNN alpha working

--- pokeFinder/index.js
+++ pokeFinder/index.js
@@ -2,8 +2,6 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { start } from "./src/app";
 
-AppRegistry.registerComponent(appName, () => App);
+start();
